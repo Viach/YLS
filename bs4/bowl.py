75e84d956c8442ebe803c5f0b1872cd43def0596
Viach <vch.380@gmail.com>
2017-07-13 12:52:14 +0300

    bs4: fix process - price-range done

--- bs4/bowl.py
+++ bs4/bowl.py
@@ -9,25 +9,34 @@ class Soup:
     def __init__(self, args):
         self.args = [param.split('=') for param in args]
         self.args = {arg[0]: arg[-1].lower() for arg in self.args if arg[0] != arg[-1]}
+        if self.args.get('--price-range', False):
+            price_range = self.args['--price-range'].split(':')
+            self.args['--price-range'] = [int(price) for price in price_range if price.isdigit()]
         self.url = 'http://price.ua/catc839t14.html'
         self.page = 1  # Number of current page.
-        self.item_list = []
+        self.item_list = []  # List with result
 
     def get_url(self, page):
+        """ Ruturn url for next scrape. """
         if page > 1:
             self.url = 'http://price.ua/catc839t14/page{}.html'.format(page)
         return self.url
 
     def get_item_detail(self, item):
+        """ Return data for every item in scrape list """
         price = item.find('div', {'class': 'price-wrap'})
         price = price.text.replace('\xa0', '')
         price = ''.join([i for i in price if i.isdigit()])
+        price_min, price_max = self.args['--price-range']
+        if price_min and price_max and price.isdigit() and price_max <= int(price) <= price_min:
+            return None
 
         name = item.find('a', {'class': {'model-name', }}).text
         name_contains = self.args.get('--name-contains', False)
         if name_contains and name_contains not in name.lower():
             return None
 
+        """ Open url for every item and scrape detailed data"""
         item_url = item.find('a', {'class': {'full-desc', }}).attrs['onmousedown'].partition('=')[-1]
         item_url = requests.get(item_url.strip('"')).content
         item_full = BeautifulSoup(item_url, "html.parser")
