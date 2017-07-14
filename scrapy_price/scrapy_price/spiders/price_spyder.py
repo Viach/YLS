04ab3edce94969ad375afa884b3caca396cd5728
Viach <vch.380@gmail.com>
2017-07-14 22:32:05 +0300

    scrapy: fix process

--- scrapy_price/scrapy_price/spiders/price_spyder.py
+++ scrapy_price/scrapy_price/spiders/price_spyder.py
@@ -13,11 +13,19 @@ class PriceSpider(Spider):
     ]
 
     def parse(self, response):
-        itemsObj = response.xpath("//div").css('.product-item').css('.view-list')
+        itemsObj = response.xpath("//div[@id='list-grid']").css('div.product-item')
         items = []
         for itemObj in itemsObj:
             item = ScrapyPriceItem()
-            item["name"] = itemObj.xpath('//div[@class="info-wrap"]').css('a.model-name')
+            item['name'] = itemObj.css('a.model-name::text').extract_first()
+            item['price'] = itemObj.css('div.price-wrap span.price::text').extract_first().replace('\xa0', '').strip()
+            item['description'] = itemObj.css('div.desc div.characteristics div.item *::text').extract()
+            item['description'] = [i.strip() for i in item['description']]
+            item['description'] = ''.join([i if i else '\n' for i in item['description']])
+            item['item_url'] = itemObj.css('div.photo-wrap a::attr(onmousedown)').extract_first()
+            item['item_photo'] = itemObj.css(
+                'div.photo-wrap div.hidden.tooltip-content::attr(data-big-image-url)').extract_first()
 
             items.append(item)
+        print(items)
         return items
