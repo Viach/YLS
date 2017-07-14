eece66b12d3790e07c2290f0bda41ce5b01d2a9e
Viach <vch.380@gmail.com>
2017-07-14 06:54:31 +0300

    full/list mode added

--- bs4/app.py
+++ bs4/app.py
@@ -1,13 +1,27 @@
-"""        Application for scraping website with BeautifulSoup    """
+"""        Application for scraping website with BeautifulSoup   
+    
+    Usage: python app.py [OPTION]... 
+
+    Options:    --start-page=<number start page>
+                --limit-pages=<number pages>
+                --price-range=<min-price>:<max-pice>
+                --name-contains=<string>
+                --mode=[list|full]
+
+
+    Example: python app.py --limit-pages=10 --price-range=10000:20000 
+                           --name-contains=Samsung --mode=list
+
+"""
 import sys
 from bowl import Soup
 
 
-def main(args):
+def main(args):    
     soup = Soup(args)
 
     item_list = soup.get_item_list()
-    print(item_list[0], len(item_list))
+    print(item_list[-1], len(item_list))
 
 
 if __name__ == '__main__':
--- bs4/bowl.py
+++ bs4/bowl.py
@@ -9,13 +9,17 @@ class Soup:
     def __init__(self, args):
         self.args = [param.split('=') for param in args]
         self.args = {arg[0]: arg[-1].lower() for arg in self.args if arg[0] != arg[-1]}
-        if self.args.get('--price-range', ''):
+        self.args.setdefault('--limit-pages', 1)
+        self.args.setdefault('--price-range', '')
+        self.args.setdefault('--mode', 'list')
+        if self.args['--price-range']:
             price_range = self.args['--price-range'].split(':')
             self.args['--price-range'] = [int(price) for price in price_range if price.isdigit()]
             self.args['--price-range'] = '?price[min]={}&price[max]={}'.format(*self.args['--price-range'])
         self.url = 'http://price.ua/catc839t14.html' + self.args['--price-range']
-        self.page = 1  # Number of current page.
+        self.page = int(self.args.get('--start-page', 1)) # Number of current page.
         self.item_list = []  # List with result
+        self.item_counter = 0
 
     def get_url(self, page):
         """ Ruturn url for next scrape. """
@@ -30,27 +34,40 @@ class Soup:
         price = ''.join([i for i in price if i.isdigit()])
 
         name = item.find('a', {'class': {'model-name', }})
-        item_url = name.get('href')
+        try:            
+            item_url = name.get('href')
+        except AttributeError as e:
+                print('\t Error Item URL for :',   '\n\t : ', e)
+                print('It seems this item is from other host. Skipped!')
+                return None
         if not item_url:  # product from other host
-            name = 'product from other host'
+            name = 'Product from other host. Skipped!'
             return name, price, None, item_url, None
         name = name.text
         name_contains = self.args.get('--name-contains', False)
         if name_contains and name_contains not in name.lower():
             return None
-
-        """ Open url for every item and scrape detailed data"""
-        r_item = requests.get(item_url)  # Get item page
-        item_full = BeautifulSoup(r_item.content, "html.parser")
-        item_description_url = item_full.find('tr', {'class': 'tr-details_compare-link'})  # Get table row with URL to descript
-        item_description_url = item_description_url.find('td', {'class': 'td-name'}).find('a').get('href')  # Get URL to descript
-        r_item_description = requests.get(item_description_url)
-        item_description = BeautifulSoup(r_item_description.content, "html.parser")
-        description = str(item_description.find('div', {'class': 'full-desc', }))
-        description = html2text.html2text(description).replace('(javascript:void\\', '').replace('[](0\\);)', '')
-        item_photos = item_description.find('div', {'id': 'scrollable-gallery-photo'})
-        item_photos = item_photos.find_all('img')
-        item_photos = [item_photo.get_attribute_list('data-original')[0] for item_photo in item_photos]
+        if self.args['--mode'].lower() == 'list':  # LIST mode
+            description = str(item.find('div', {'class':'characteristics'}))
+            item_photos = item.find('div', {'class':'photo-wrap'}).find('img').get('src')
+        else:
+            """ Open url for every item and scrape detailed data - FULL mode"""
+            r_item = requests.get(item_url)  # Get item page
+            item_full = BeautifulSoup(r_item.content, "html.parser")
+            try:
+                item_description_url = item_full.find('tr', {'class': 'tr-details_compare-link'})  # Get table row with URL to descript
+                item_description_url = item_description_url.find('td', {'class': 'td-name'}).find('a').get('href')  # Get URL to descript
+                r_item_description = requests.get(item_description_url)
+            except BaseException as e:
+                print('\t Error Item description URL for :', name, item_url, '\n\t : ', e)
+                return None
+            item_description = BeautifulSoup(r_item_description.content, "html.parser")
+            description = str(item_description.find('div', {'class': 'full-desc', }))
+            description = html2text.html2text(description).replace('(javascript:void\\', '').replace('[](0\\);)', '')
+            item_photos = item_description.find('div', {'id': 'scrollable-gallery-photo'}).find_all('img')            
+            item_photos = [item_photo.get_attribute_list('data-original')[0] for item_photo in item_photos]
+        self.item_counter += 1
+        print('\t',self.item_counter, ' : ', name, item_url, ' parsed')
         return name, price, description, item_url, item_photos
 
     def get_item_list(self):
@@ -67,8 +84,8 @@ class Soup:
             if item_detail:
                 self.item_list.append(item_detail)
         print('Scanned {} page of {} pages (max pages for scan: {})'.format(self.page, max_page,
-                                                                            self.args['--max-pages']))
-        if self.page < max_page and self.page < int(self.args['--max-pages']):
+                                                                            self.args['--limit-pages']))
+        if self.page < max_page and self.page < int(self.args['--limit-pages']):
             self.page += 1
             self.get_item_list()
 
